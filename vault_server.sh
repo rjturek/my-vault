2ccdefe08f86cf995b5bcec52f0fe3f4a1b2235a
Richard Turek <richard_turek@troweprice.com>
2019-04-15 17:57:14 -0400

    more

--- vault_server.sh
+++ vault_server.sh
@@ -2,40 +2,48 @@
 
 usage() {
   echo "Usage: "
-  echo "  $0 show " 
-  echo "  $0 start   <instance>"
-  echo "  $0 stop    <instance>"
-  echo "  $0 setvars <instance>" 
+  echo "  $0 <command> [instance]" 
+  echo "Commands"
   echo "  show             : Show running Vault servers and environment variables"
   echo "  start <instance> : Start vault instance* if not already started; set env vars to this instance."
   echo "  stop  <instance> : Stop vault instance* if it is running."
-  echo '*Note: Instance name "dev" is special, causing vault tp start in dev mode.'
-  echo "       Other instance names have a corresponding directory containing configuration, etc."
+  echo "  set   <instance> : Set variables to the instance."
+  echo '*Note: Instance name "dev" is special, causing vault to start in dev mode.'
+  echo '       Other instance names have corresponding directory with name = "instance_<instance-name>, '
+  echo "       containing configuration, etc."
 }
 
 show() {
   echo " "
+  echo "Environment Variables:"
+  echo "  VAULT_ADDRESS: $VAULT_ADDRESS"
+  echo "  VAULT_TOKEN:   $VAULT_TOKEN   (Probably best if this is not set)"
+
+  local_servers
+}
+
+local_servers() {
   echo " "
+  echo "Running Vault servers:"
+
+  # Get all vault process pids
+  VAULT_PS_PIDS=$(ps | grep 'vault' | grep 'server' | grep -v $0 | awk '{print $1}' )
   
-  PS_RESULT=$(ps -ef | grep 'vault' | grep 'server' | grep '\-dev' | grep -v $0)
-  if [ $? -ne 0 ]; then
-    echo "Server not running in dev mode."
-  else
-    export PID=$(echo $PS_RESULT | awk '{print $2}' )
-    echo "Server is running in dev mode.  Process PID: $PID"
-  fi
+  # Loop over each process, pull out instance name 
+  for pid in $VAULT_PS_PIDS; do
+    echo $pid
+    executable=$(ps -p $pid | awk 'NR>1 {print $4}')  
+    first_arg=$(ps -p  $pid | awk 'NR>1 {print $5}')
+    second_arg=$(ps -p $pid | awk 'NR>1 {print $6}')
+    third_arg=$(ps -p $pid | awk 'NR>1 {print $7}')
+    if [[ "$executable" == "vault" && "$first_arg" == "server" ]]; then 
+      echo "  $second_arg $third_arg (PID $pid)"
+    fi
+  done 
 }
 
 start() {
-  show_current
-  if [[ "$PID" != "" ]]; then
-    echo "Skipping start."
-  else 
-    echo "Starting vault in dev mode ..."
-    vault server -dev > $START_INFO_FILE
-    sleep 3
-    show_current
-  fi
+  
 }
 
 stop() {
@@ -60,13 +68,22 @@ if [ $# -eq 0 ]; then
   usage
 elif [ $# -eq 1 ]; then
   if [[ "$1" == "show" ]]; then 
-  
-  usage
-elif [[ "start" == "$1" ]]; then 
-  start
-elif [[ "stop" == "$1" ]]; then 
-  stop
-else 
+    show
+  else
+    usage
+  fi
+elif [ $# -eq 2 ]; then
+  INSTANCE_ARG=$2
+  if [[ "$1" == "start" ]]; then
+    start
+  elif [[ "stop" == "$1" ]]; then 
+    stop
+  elif [[ "setvars" == "$1" ]]; then 
+    setvars
+  else 
+    usage
+  fi
+else
   usage
 fi
 
