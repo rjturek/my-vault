e6f56c064172524dd9e65b8a46a3ae94ab21c17a
Richard Turek <richard_turek@troweprice.com>
2019-04-16 14:37:12 -0400

    show w/ split

--- vault_server.sh
+++ vault_server.sh
@@ -15,35 +15,39 @@ usage() {
 
 show() {
   echo " "
-  echo "Environment Variables:"
+  echo "Current Vault Environment Variables:"
   echo "  VAULT_ADDRESS: $VAULT_ADDRESS"
-  echo "  VAULT_TOKEN:   $VAULT_TOKEN   (Probably best if this is not set)"
+  echo "  VAULT_TOKEN:   $VAULT_TOKEN   (Should this be set?)"
+  echo "  VAULT_CACERT:  $VAULT_CACERT"
 
-  local_servers
+  RUNNING_SERVERS=$(local_servers_running)
+  echo " "
+  echo "Running Vault servers <PID> <instance-name>"
+  for svr in $RUNNING_SERVERS; do  
+    echo $svr | cut -d'|' -f1,2,3 | tr '|' ' '
+  done
+  echo " "
 }
 
-local_servers() {
-  echo " "
-  echo "Running Vault servers:"
+local_servers_running() {
 
   # Get all vault process pids
   VAULT_PS_PIDS=$(ps | grep 'vault' | grep 'server' | grep -v $0 | awk '{print $1}' )
   
   # Loop over each process, pull out instance name 
   for pid in $VAULT_PS_PIDS; do
-    echo $pid
     executable=$(ps -p $pid | awk 'NR>1 {print $4}')  
     first_arg=$(ps -p  $pid | awk 'NR>1 {print $5}')
     second_arg=$(ps -p $pid | awk 'NR>1 {print $6}')
     third_arg=$(ps -p $pid | awk 'NR>1 {print $7}')
     if [[ "$executable" == "vault" && "$first_arg" == "server" ]]; then 
-      echo "  $second_arg $third_arg (PID $pid)"
+      echo "$pid|$second_arg|$third_arg)"
     fi
   done 
 }
 
 start() {
-  
+  echo "start TODO"
 }
 
 stop() {
@@ -61,8 +65,7 @@ stop() {
 # DATE=$(date "+%Y-%m-%d_%H:%M:%S")
 # START_INFO_FILE="/tmp/vault-$DATE"
 
-PID=""
-INSTANCE_ARG="unset"
+unset INSTANCE_ARG
 
 if [ $# -eq 0 ]; then
   usage
